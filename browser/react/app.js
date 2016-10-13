8f2ac42021a8be2edea6969e055fab9de1c3eeef
David Y. Kim <davidykim@gmail.com>
2016-10-13 13:04:22 -0400

    lunch

--- browser/react/app.js
+++ browser/react/app.js
@@ -5,10 +5,28 @@ import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
 import AppContainer from './containers/AppContainer';
+import ArtistsContainer from './containers/ArtistsContainer';
+import AlbumsContainer from './containers/AlbumsContainer';
+import Album from "./components/Album.js";
+
+import {Router, Route, hashHistory, IndexRedirect} from "react-router";
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer /> 
+    <Router history={hashHistory}>
+      <Route path="/" component={AppContainer}>
+        <IndexRedirect to="/albums"/>
+        <Route path="/albums" component= {AlbumsContainer}/>
+        <Route path="/albums/:albumId" component= {Album}/>
+
+        <Route path="/artists" component= {ArtistsContainer}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
+
+
+        // <IndexRoute component= {AlbumsContainer}/>
+
--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -2,13 +2,27 @@
 
 import React from 'react';
 import SongsContainer from '../containers/SongsContainer';
+import { fetchAndGoToAlbum } from "../action-creators/albums.js"
 
-export default ({ selectedAlbum }) => (
-  <div className="album">
-    <div>
-      <h3>{ selectedAlbum.name }</h3>
-      <img src={ selectedAlbum.imageUrl } className="img-thumbnail" />
-    </div>
-    <SongsContainer songs={selectedAlbum.songs} />
-  </div>
-);
+export default class Album extends React.Component {
+
+  componentDidMount () {
+    fetchAndGoToAlbum({id: req.params.albumId});
+  }
+
+
+  render () {
+    selectedAlbum = this.props.selectedAlbum;
+
+    return (
+      <div className="album">
+        <div>
+          <h3>{ selectedAlbum.name }</h3>
+          <img src={ selectedAlbum.imageUrl } className="img-thumbnail" />
+        </div>
+        <SongsContainer songs={selectedAlbum.songs} />
+      </div>
+      )
+  }
+
+}
